a574f99e12467b0674a5dc6558eb8afc268c28c7
Aaron.Lee <auneeyy@gmail.com>
2016-07-18 11:54:41 +0800

    Format

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ var converter = new Converter({});
 
 //end_parsed will be emitted once parsing finished 
 converter.on("end_parsed", function(citys) {
-	var cityConstants = {
-		CITYDICT: {},
-		CITYLIST: []
-	};
+  var cityConstants = {
+    CITYDICT: {},
+    CITYLIST: []
+  };
 
-	citys.forEach(function(city) {
-		cityConstants.CITYDICT[city.id] = city.name;
-		cityConstants.CITYLIST.push({
-			id: city.id,
-			name: city.name
-		});
-	});
+  citys.forEach(function(city) {
+    cityConstants.CITYDICT[city.id] = city.name;
+    cityConstants.CITYLIST.push({
+      id: city.id,
+      name: city.name
+    });
+  });
 
-	fs.writeFile('city.json', util.format('%j', cityConstants), (err) => {
-		if (err) throw err;
-		console.log('It\'s saved!');
-	});
+  fs.writeFile('city.json', util.format('%j', cityConstants), (err) => {
+    if (err) throw err;
+    console.log('It\'s saved!');
+  });
 });
 
 //read from file 
